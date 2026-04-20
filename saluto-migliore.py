0fd9308c98108036c37f2bd6831a86d0e3d7c392
Francesco Antonini <antoninifrancesco91@gmail.com>
2026-04-20 16:13:24 +0200

    modifiche

--- saluto-migliore.py
+++ saluto-migliore.py
@@ -1,4 +1,11 @@
 def saluta_avanzato(saluto, nome): 
     return f"{saluto}, {nome}!" 
  
-print(saluta("Ciao", "Mondo")) 
+print(saluta_avanzato("Ciao", "Mondo")) 
+
+def arrivederci(nome):
+    return f"Arrivederci, {nome}."
+
+print(arrivederci("Matteo"))
+
+
